4d373b5c270ef0fe680e78a9b24a3db86c7cbe27
Matthew Chan <matthew.chan@prizmmedia.com>
2017-09-28 18:54:17 +0100

    Added some functions to DrawerSidebar and DrawerView which allows the drawer to be updated in real time as user information changes.

--- src/views/Drawer/DrawerSidebar.js
+++ src/views/Drawer/DrawerSidebar.js
@@ -1,125 +1,181 @@
 /* @flow */
 
-import React, { PureComponent } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {PureComponent} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 import withCachedChildNavigation from '../../withCachedChildNavigation';
 import NavigationActions from '../../NavigationActions';
 
+import _ from 'lodash';
+
 import type {
-  NavigationScreenProp,
-  NavigationRoute,
-  NavigationAction,
-  NavigationRouter,
-  NavigationDrawerScreenOptions,
-  NavigationState,
-  NavigationStateRoute,
-  Style,
+    NavigationScreenProp,
+    NavigationRoute,
+    NavigationAction,
+    NavigationRouter,
+    NavigationDrawerScreenOptions,
+    NavigationState,
+    NavigationStateRoute,
+    Style,
 } from '../../TypeDefinition';
 
-import type { DrawerScene, DrawerItem } from './DrawerView';
+import type {DrawerScene, DrawerItem} from './DrawerView';
 
-type Navigation = NavigationScreenProp<NavigationStateRoute, NavigationAction>;
+type
+Navigation = NavigationScreenProp < NavigationStateRoute, NavigationAction >;
 
-type Props = {
-  router: NavigationRouter<
-    NavigationState,
-    NavigationAction,
-    NavigationDrawerScreenOptions
-  >,
-  navigation: Navigation,
-  childNavigationProps: { [key: string]: Navigation },
-  contentComponent: ReactClass<*>,
-  contentOptions?: {},
-  screenProps?: {},
-  style?: Style,
-};
+type
+Props = {
+        router: NavigationRouter <
+        NavigationState,
+        NavigationAction,
+        NavigationDrawerScreenOptions
+    >,
+    navigation
+:
+Navigation,
+    childNavigationProps
+:
+{
+    [key
+:
+    string
+]:
+    Navigation
+}
+,
+contentComponent: ReactClass < * >,
+    contentOptions ? : {},
+    screenProps ? : {},
+    style ? : Style,
+}
+;
 
 /**
  * Component that renders the sidebar screen of the drawer.
  */
 class DrawerSidebar extends PureComponent<void, Props, void> {
-  props: Props;
+    props: Props;
 
-  _getScreenOptions = (routeKey: string) => {
-    const DrawerScreen = this.props.router.getComponentForRouteName(
-      'DrawerClose'
-    );
-    const { [routeKey]: childNavigation } = this.props.childNavigationProps;
-    return DrawerScreen.router.getScreenOptions(
-      childNavigation.state.index !== undefined // if the child screen is a StackRouter then always show the screen options of its first screen (see #1914)
-        ? {
-            ...childNavigation,
-            state: { ...childNavigation.state, index: 0 },
-          }
-        : childNavigation,
-      this.props.screenProps
-    );
-  };
+    constructor(props) {
+        super(props);
 
-  _getLabel = ({ focused, tintColor, route }: DrawerScene) => {
-    const { drawerLabel, title } = this._getScreenOptions(route.key);
-    if (drawerLabel) {
-      return typeof drawerLabel === 'function'
-        ? drawerLabel({ tintColor, focused })
-        : drawerLabel;
+        this.state = {
+            fname: null,
+            rebate_total: null
+        }
+
+        //this.updateDrawerUserInfo = this.updateDrawerUserInfo.bind(this);
     }
 
-    if (typeof title === 'string') {
-      return title;
+    _getScreenOptions = (routeKey: string) => {
+        const DrawerScreen = this.props.router.getComponentForRouteName(
+            'DrawerClose'
+        );
+        const {[routeKey]: childNavigation} = this.props.childNavigationProps;
+        return DrawerScreen.router.getScreenOptions(
+            childNavigation.state.index !== undefined // if the child screen is a StackRouter then always show the screen options of its first screen (see #1914)
+                ? {
+                    ...childNavigation,
+                    state: {...childNavigation.state, index: 0},
+                }
+                : childNavigation,
+            this.props.screenProps
+        );
+    };
+
+    _getLabel = ({focused, tintColor, route}: DrawerScene) => {
+        const {drawerLabel, title} = this._getScreenOptions(route.key);
+        if (drawerLabel) {
+            return typeof drawerLabel === 'function'
+                ? drawerLabel({tintColor, focused})
+                : drawerLabel;
+        }
+
+        if (typeof title === 'string') {
+            return title;
+        }
+
+        return route.routeName;
+    };
+
+    _renderIcon = ({focused, tintColor, route}: DrawerScene) => {
+        const {drawerIcon} = this._getScreenOptions(route.key);
+        if (drawerIcon) {
+            return typeof drawerIcon === 'function'
+                ? drawerIcon({tintColor, focused})
+                : drawerIcon;
+        }
+        return null;
+    };
+
+    _onItemPress = ({route, focused}: DrawerItem) => {
+        this.props.navigation.navigate('DrawerClose');
+        if (!focused) {
+            const subAction = route.index !== undefined && route.index !== 0 // if the child screen is a StackRouter then always navigate to its first screen (see #1914)
+                ? NavigationActions.navigate({routeName: route.routes[0].routeName})
+                : undefined;
+            this.props.navigation.navigate(route.routeName, undefined, subAction);
+        }
+    };
+
+    componentDidMount() {
+        //console.log('mounted');
+        //this.updateDrawerUserInfo();
+        this.props.helperFunctions.updateSidebar = this.updateDrawerUserInfo.bind(this);
     }
 
-    return route.routeName;
-  };
+    /*componentWillReceiveProps(newProps) {
+      console.log('new props');
+      console.log(newProps);
+      this.updateDrawerUserInfo(newProps);
+    }*/
 
-  _renderIcon = ({ focused, tintColor, route }: DrawerScene) => {
-    const { drawerIcon } = this._getScreenOptions(route.key);
-    if (drawerIcon) {
-      return typeof drawerIcon === 'function'
-        ? drawerIcon({ tintColor, focused })
-        : drawerIcon;
+    updateDrawerUserInfo(info) {
+        const {
+            fName, rebateTotal
+        }
+            = info;
+        //console.log('update drawer');
+
+        let newState = this.state;
+        newState.fname = fName;
+        newState.rebate_total = rebateTotal;
+        this.setState(newState);
+        this.forceUpdate();
     }
-    return null;
-  };
 
-  _onItemPress = ({ route, focused }: DrawerItem) => {
-    this.props.navigation.navigate('DrawerClose');
-    if (!focused) {
-      const subAction = route.index !== undefined && route.index !== 0 // if the child screen is a StackRouter then always navigate to its first screen (see #1914)
-        ? NavigationActions.navigate({ routeName: route.routes[0].routeName })
-        : undefined;
-      this.props.navigation.navigate(route.routeName, undefined, subAction);
+    render() {
+        const ContentComponent = this.props.contentComponent;
+        const {state} = this.props.navigation;
+        //console.log('drawer sidebar render');
+        return (
+            <View style={[styles.container, this.props.style]}>
+              <ContentComponent
+                  {...this.props.contentOptions}
+                  navigation={this.props.navigation}
+                  items={state.routes}
+                  activeItemKey={
+                      state.routes[state.index] && state.routes[state.index].key
+                  }
+                  screenProps={this.props.screenProps}
+                  getLabel={this._getLabel}
+                  renderIcon={this._renderIcon}
+                  onItemPress={this._onItemPress}
+                  router={this.props.router}
+                  fname={this.state.fname}
+                  rebate_total={this.state.rebate_total}
+              />
+            </View>
+        );
     }
-  };
-
-  render() {
-    const ContentComponent = this.props.contentComponent;
-    const { state } = this.props.navigation;
-    return (
-      <View style={[styles.container, this.props.style]}>
-        <ContentComponent
-          {...this.props.contentOptions}
-          navigation={this.props.navigation}
-          items={state.routes}
-          activeItemKey={
-            state.routes[state.index] && state.routes[state.index].key
-          }
-          screenProps={this.props.screenProps}
-          getLabel={this._getLabel}
-          renderIcon={this._renderIcon}
-          onItemPress={this._onItemPress}
-          router={this.props.router}
-        />
-      </View>
-    );
-  }
 }
 
 export default withCachedChildNavigation(DrawerSidebar);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
--- src/views/Drawer/DrawerView.js
+++ src/views/Drawer/DrawerView.js
@@ -1,157 +1,190 @@
 /* @flow */
 
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import DrawerLayout from 'react-native-drawer-layout-polyfill';
 
 import addNavigationHelpers from '../../addNavigationHelpers';
 import DrawerSidebar from './DrawerSidebar';
 
+import _ from 'lodash';
+
 import type {
-  NavigationScreenProp,
-  NavigationRoute,
-  NavigationRouter,
-  NavigationState,
-  NavigationAction,
-  NavigationDrawerScreenOptions,
-  Style,
-} from '../../TypeDefinition';
-
-export type DrawerScene = {
-  route: NavigationRoute,
-  focused: boolean,
-  index: number,
-  tintColor?: string,
-};
-
-export type DrawerItem = {
-  route: NavigationRoute,
-  focused: boolean,
-};
-
-export type DrawerViewConfig = {
-  drawerWidth: number,
-  drawerPosition: 'left' | 'right',
-  contentComponent: ReactClass<*>,
-  contentOptions?: {},
-  style?: Style,
-};
-
-type Props = DrawerViewConfig & {
-  screenProps?: {},
-  router: NavigationRouter<
+    NavigationScreenProp,
+    NavigationRoute,
+    NavigationRouter,
     NavigationState,
     NavigationAction,
-    NavigationDrawerScreenOptions
-  >,
-  navigation: NavigationScreenProp<NavigationState, NavigationAction>,
+    NavigationDrawerScreenOptions,
+    Style,
+} from '../../TypeDefinition';
+
+export
+type
+DrawerScene = {
+    route: NavigationRoute,
+    focused: boolean,
+    index: number,
+    tintColor? : string,
 };
+
+export
+type
+DrawerItem = {
+    route: NavigationRoute,
+    focused: boolean,
+};
+
+export
+type
+DrawerViewConfig = {
+    drawerWidth: number,
+    drawerPosition: 'left' | 'right',
+    contentComponent: ReactClass < * >,
+    contentOptions? : {},
+    style? : Style,
+};
+
+type
+Props = DrawerViewConfig & {
+        screenProps? : {},
+        router: NavigationRouter <
+        NavigationState,
+        NavigationAction,
+        NavigationDrawerScreenOptions
+    >,
+    navigation
+:
+NavigationScreenProp < NavigationState, NavigationAction >,
+}
+;
 
 /**
  * Component that renders the drawer.
  */
 export default class DrawerView<T: *> extends PureComponent<void, Props, void> {
-  props: Props;
+    props: Props;
+    helperFunctions;
 
-  componentWillMount() {
-    this._updateScreenNavigation(this.props.navigation);
-  }
+    constructor() {
+        super();
 
-  componentWillReceiveProps(nextProps: Props) {
-    if (
-      this.props.navigation.state.index !== nextProps.navigation.state.index
-    ) {
-      const { routes, index } = nextProps.navigation.state;
-      if (routes[index].routeName === 'DrawerOpen') {
-        this._drawer.openDrawer();
-      } else {
-        this._drawer.closeDrawer();
-      }
+        this.getLatestUserData = this.getLatestUserData.bind(this);
+
+        this.helperFunctions = {
+            updateSidebar: null
+        };
     }
-    this._updateScreenNavigation(nextProps.navigation);
-  }
 
-  _screenNavigationProp: NavigationScreenProp<T, NavigationAction>;
-
-  _handleDrawerOpen = () => {
-    const { navigation } = this.props;
-    const { routes, index } = navigation.state;
-    if (routes[index].routeName !== 'DrawerOpen') {
-      this.props.navigation.navigate('DrawerOpen');
+    componentWillMount() {
+        this._updateScreenNavigation(this.props.navigation);
     }
-  };
 
-  _handleDrawerClose = () => {
-    const { navigation } = this.props;
-    const { routes, index } = navigation.state;
-    if (routes[index].routeName !== 'DrawerClose') {
-      this.props.navigation.navigate('DrawerClose');
-    }
-  };
-
-  _updateScreenNavigation = (
-    navigation: NavigationScreenProp<NavigationState, NavigationAction>
-  ) => {
-    const navigationState = navigation.state.routes.find(
-      (route: *) => route.routeName === 'DrawerClose'
-    );
-    if (
-      this._screenNavigationProp &&
-      this._screenNavigationProp.state === navigationState
-    ) {
-      return;
-    }
-    this._screenNavigationProp = addNavigationHelpers({
-      ...navigation,
-      state: navigationState,
-    });
-  };
-
-  _getNavigationState = (
-    navigation: NavigationScreenProp<NavigationState, NavigationAction>
-  ) => {
-    const navigationState = navigation.state.routes.find(
-      (route: *) => route.routeName === 'DrawerClose'
-    );
-    return navigationState;
-  };
-
-  _renderNavigationView = () => (
-    <DrawerSidebar
-      screenProps={this.props.screenProps}
-      navigation={this._screenNavigationProp}
-      router={this.props.router}
-      contentComponent={this.props.contentComponent}
-      contentOptions={this.props.contentOptions}
-      style={this.props.style}
-    />
-  );
-
-  _drawer: any;
-
-  render() {
-    const DrawerScreen = this.props.router.getComponentForRouteName(
-      'DrawerClose'
-    );
-    return (
-      <DrawerLayout
-        ref={(c: *) => {
-          this._drawer = c;
-        }}
-        drawerWidth={this.props.drawerWidth}
-        onDrawerOpen={this._handleDrawerOpen}
-        onDrawerClose={this._handleDrawerClose}
-        renderNavigationView={this._renderNavigationView}
-        drawerPosition={
-          this.props.drawerPosition === 'right'
-            ? DrawerLayout.positions.Right
-            : DrawerLayout.positions.Left
+    componentWillReceiveProps(nextProps: Props) {
+        if (this.helperFunctions.updateSidebar !== null)
+            this.helperFunctions.updateSidebar(this.getLatestUserData(nextProps));
+        if (
+            this.props.navigation.state.index !== nextProps.navigation.state.index
+        ) {
+            const {routes, index} = nextProps.navigation.state;
+            if (routes[index].routeName === 'DrawerOpen') {
+                this._drawer.openDrawer();
+            } else {
+                this._drawer.closeDrawer();
+            }
         }
-      >
-        <DrawerScreen
-          screenProps={this.props.screenProps}
-          navigation={this._screenNavigationProp}
+        this._updateScreenNavigation(nextProps.navigation);
+    }
+
+    _screenNavigationProp: NavigationScreenProp<T, NavigationAction>;
+
+    _handleDrawerOpen = () => {
+        const {navigation} = this.props;
+        const {routes, index} = navigation.state;
+        if (routes[index].routeName !== 'DrawerOpen') {
+            this.props.navigation.navigate('DrawerOpen');
+        }
+    };
+
+    _handleDrawerClose = () => {
+        const {navigation} = this.props;
+        const {routes, index} = navigation.state;
+        if (routes[index].routeName !== 'DrawerClose') {
+            this.props.navigation.navigate('DrawerClose');
+        }
+    };
+
+    _updateScreenNavigation = (navigation: NavigationScreenProp<NavigationState, NavigationAction>) => {
+        const navigationState = navigation.state.routes.find(
+            (route: *) => route.routeName === 'DrawerClose'
+        );
+        if (
+            this._screenNavigationProp &&
+            this._screenNavigationProp.state === navigationState
+        ) {
+            return;
+        }
+        this._screenNavigationProp = addNavigationHelpers({
+            ...navigation,
+            state: navigationState,
+        });
+    };
+
+    _getNavigationState = (navigation: NavigationScreenProp<NavigationState, NavigationAction>) => {
+        const navigationState = navigation.state.routes.find(
+            (route: *) => route.routeName === 'DrawerClose'
+        );
+        return navigationState;
+    };
+
+    getLatestUserData = (props) => {
+        if (!props) {
+            props = this.props;
+        }
+        return {
+            fName: _.get(props, 'navigation.screenProps.user.data.fName', ''),
+            rebateTotal: _.get(props, 'navigation.screenProps.user.rebate_total', '0.00')
+        }
+    }
+
+    _renderNavigationView = () => (
+        <DrawerSidebar
+            screenProps={this.props.screenProps}
+            getLatestUserData={this.getLatestUserData}
+            helperFunctions={this.helperFunctions}
+            navigation={this._screenNavigationProp}
+            router={this.props.router}
+            contentComponent={this.props.contentComponent}
+            contentOptions={this.props.contentOptions}
+            style={this.props.style}
         />
-      </DrawerLayout>
     );
-  }
+
+    _drawer: any;
+
+    render() {
+        const DrawerScreen = this.props.router.getComponentForRouteName(
+            'DrawerClose'
+        );
+        return (
+            <DrawerLayout
+                ref={(c: *) => {
+                    this._drawer = c;
+                }}
+                drawerWidth={this.props.drawerWidth}
+                onDrawerOpen={this._handleDrawerOpen}
+                onDrawerClose={this._handleDrawerClose}
+                renderNavigationView={this._renderNavigationView}
+                drawerPosition={
+                    this.props.drawerPosition === 'right'
+                        ? DrawerLayout.positions.Right
+                        : DrawerLayout.positions.Left
+                }
+            >
+              <DrawerScreen
+                  screenProps={this.props.screenProps}
+                  navigation={this._screenNavigationProp}
+              />
+            </DrawerLayout>
+        );
+    }
 }
